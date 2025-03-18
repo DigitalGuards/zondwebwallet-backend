3d1963e1c4df49f18165683a8c22a6a567f66893
omega <hanzhang.tw@gmail.com>
2025-03-18 22:20:54 +0900

    feat: impelemnt support email endpoint

---
+++ src/routes/app.routes.js
@@ -0,0 +1,67 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const appRouter = express.Router();
+
+appRouter.post("/support", (req, res) => {
+    const { name, email, message, subject } = req.body;
+
+    if (!name || !email || !message || !subject) {
+        return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_TOKEN
+        }
+    });
+
+    const mailOptions = {
+        from: process.env.SMTP_FROM,
+        to: process.env.SMTP_TO,
+        subject: subject,
+        html: `<h1>Support Request</h1>
+               <p>Dear Support Team,</p>
+               <p>You have received a new support request from <strong>${name}</strong>. Please find the details below:</p>
+               <p><strong>Message:</strong> ${message}</p>
+               <p>Kindly address this request at your earliest convenience.</p>
+               <p>Best regards,</p>
+               <p>TheQRLWallet Team</p>
+               <p>Email: ${process.env.SMTP_FROM}</p>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).json({ message: "Failed to send support request email" });
+        } else {
+            console.log("Support email sent: " + info.response);
+            const confirmationMailOptions = {
+                from: process.env.SMTP_FROM,
+                to: email,
+                subject: "Confirmation of Your Support Request",
+                html: `<h1>Support Request Confirmation</h1>
+                       <p>Dear ${name},</p>
+                       <p>Thank you for reaching out to our support team. We have received your request and our team will get back to you shortly.</p>
+                       <p>We appreciate your patience and understanding.</p>
+                       <p>Best regards,</p>
+                       <p>TheQRLWallet Team</p>
+                       <p>Email: ${process.env.SMTP_FROM}</p>`
+            };
+            transporter.sendMail(confirmationMailOptions, (error, info) => {
+                if (error) {
+                    console.log("Error sending confirmation email: ", error);
+                    return res.status(500).json({ message: "Failed to send confirmation email" });
+                } else {
+                    console.log("Confirmation email sent: " + info.response);
+                    res.status(200).json({ message: "Support request sent successfully to " + process.env.SMTP_TO });
+                }
+            });
+        }
+    });
+});
+
+export default appRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { healthRoutes } from './health.routes.js';
 import { rpcRoutes } from './rpc.routes.js';
+import appRouter from './app.routes.js';
 
 const router = Router();
 
 router.use('/health', healthRoutes);
 router.use('/api/zond-rpc', rpcRoutes);
+router.use('/api', appRouter);
 
 export const routes = router; 
